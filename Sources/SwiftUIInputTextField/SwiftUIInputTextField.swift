220e8a5d4ade505d1ae5dfe8918cfa39d3d96775
Ram <rnrrajput84@gmail.com>
2024-02-12 12:11:14 +0530

    Initial Commit

---
+++ Sources/SwiftUIInputTextField/SwiftUIInputTextField.swift
@@ -0,0 +1,52 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import SwiftUI
+
+@available(macOS 10.15, *)
+public struct SwiftUIInputTextField: View {
+    //MARK:- PROPERTIES
+    public var placeholder: Text?
+    public var fontName: String?
+    public var fontSize: CGFloat?
+    public var fontColor: Color?
+    public var foregroundColor: Color?
+    @Binding public var textInput: String
+    public var editingChanged: (Bool)->() = { _ in }
+    public var commit: ()->() = { }
+
+    public init(placeholder: Text, fontName: String?, fontSize: CGFloat?, fontColor: Color?, foregroundColor: Color? = nil, textInput: Binding<String>, editingChanged: @escaping (Bool) -> Void, commit: @escaping () -> Void) {
+        self.placeholder = placeholder
+        self.fontName = fontName
+        self.fontSize = fontSize
+        self.fontColor = fontColor
+        self.foregroundColor = foregroundColor
+        _textInput = textInput
+        self.editingChanged = editingChanged
+        self.commit = commit
+    }
+    
+    public var body: some View {
+        ZStack(alignment: .leading) {
+            if (textInput.isEmpty) {
+                placeholder.modifier(CustomTextM(fontName: fontName ?? "", fontSize: fontSize ?? 12, fontColor: .gray))
+            }
+            TextField("", text: $textInput, onEditingChanged: editingChanged, onCommit: commit).foregroundColor((foregroundColor != nil) ? foregroundColor : .black).autocorrectionDisabled()
+                .clipShape(Capsule())
+        }
+    }
+}
+@available(macOS 10.15, *)
+struct CustomTextM: ViewModifier {
+    //MARK:- PROPERTIES
+    let fontName: String
+    let fontSize: CGFloat
+    let fontColor: Color
+     func body(content: Content) -> some View {
+        content
+            .font(.custom(fontName, size: fontSize))
+            .foregroundColor(fontColor)
+    }
+}
+
+//:completeSettings = none
