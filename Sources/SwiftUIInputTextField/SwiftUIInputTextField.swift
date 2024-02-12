b3471e3b111df4589134bf50ed24bda562eae6f3
Ram <rnrrajput84@gmail.com>
2024-02-12 23:44:53 +0530

    ramm

--- Sources/SwiftUIInputTextField/SwiftUIInputTextField.swift
+++ Sources/SwiftUIInputTextField/SwiftUIInputTextField.swift
@@ -14,23 +14,37 @@ public struct SwiftUIInputTextField: View {
     @Binding public var textInput: String
     public var editingChanged: (Bool)->() = { _ in }
     public var commit: ()->() = { }
+    public var isPassword: Bool? = false
+    public var height: Float? = 45
 
-    public init(placeholder: String, fontName: String?, fontSize: CGFloat?, fontColor: Color?, foregroundColor: Color? = nil, textInput: Binding<String>) {
+    public init(placeholder: String, fontName: String?, fontSize: CGFloat?, fontColor: Color?, foregroundColor: Color? = nil,height: Float?, isPassword: Bool?, textInput: Binding<String>) {
         self.placeholder = placeholder
         self.fontName = fontName
         self.fontSize = fontSize
         self.fontColor = fontColor
         self.foregroundColor = foregroundColor
+        self.height = height
+        self.isPassword = isPassword
         _textInput = textInput
     }
     
     public var body: some View {
         ZStack(alignment: .leading) {
-            TextField(placeholder ?? "", text: $textInput, onEditingChanged: editingChanged, onCommit: commit).foregroundColor((foregroundColor != nil) ? foregroundColor : .black).autocorrectionDisabled()
-                .textFieldStyle(.roundedBorder)
-                .frame(height: 42)
-                .padding(.leading,5)
-                .padding(.trailing,5)
+            if self.isPassword == true {
+                SecureField(placeholder ?? "", text: $textInput, onCommit: commit)
+                    .foregroundColor((foregroundColor != nil) ? foregroundColor : .black).autocorrectionDisabled()
+                    .textFieldStyle(.roundedBorder)
+                    .frame(height: CGFloat(height ?? 45))
+                    .padding(.leading,5)
+                    .padding(.trailing,5)
+            } else {
+                TextField(placeholder ?? "", text: $textInput, onEditingChanged: editingChanged, onCommit: commit)
+                    .foregroundColor((foregroundColor != nil) ? foregroundColor : .black).autocorrectionDisabled()
+                    .textFieldStyle(.roundedBorder)
+                    .frame(height: CGFloat(height ?? 45))
+                    .padding(.leading,5)
+                    .padding(.trailing,5)
+            }
         }
     }
 }
