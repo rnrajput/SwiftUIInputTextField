b73073b08a12e0106730898507fb0c760e6e4a85
Ram <rnrrajput84@gmail.com>
2024-02-13 22:11:40 +0530

    sss

--- Sources/SwiftUIInputTextField/SwiftUIInputTextField.swift
+++ Sources/SwiftUIInputTextField/SwiftUIInputTextField.swift
@@ -3,25 +3,25 @@
 
 import SwiftUI
 
-@available(macOS 15.0, *)
+
+@available(macOS 13.15, *)
 public struct SwiftUIInputTextField: View {
     //MARK:- PROPERTIES
-    public var placeholder: String?
     public var fontName: String?
     public var fontSize: CGFloat?
-    public var fontColor: Color?
     public var foregroundColor: Color?
-    @Binding public var textInput: String
     public var editingChanged: (Bool)->() = { _ in }
     public var commit: ()->() = { }
-    public var isPassword: Bool? = false
     public var height: Float? = 45
-
+    public var placeholder: String?
+    public var isPassword: Bool? = false
+    @Binding public var textInput: String
+    public var textCase: Text.Case?
+    
     public init(placeholder: String, fontName: String?, fontSize: CGFloat?, fontColor: Color?, foregroundColor: Color? = nil,height: Float = 0.0, isPassword: Bool = false, textInput: Binding<String>) {
         self.placeholder = placeholder
         self.fontName = fontName
         self.fontSize = fontSize
-        self.fontColor = fontColor
         self.foregroundColor = foregroundColor
         self.height = height
         self.isPassword = isPassword
@@ -32,29 +32,52 @@ public struct SwiftUIInputTextField: View {
         ZStack(alignment: .leading) {
             if self.isPassword == true {
                 SecureField(placeholder ?? "", text: $textInput, onCommit: commit)
-                    .textCase(.lowercase)
-                    .font(.custom(fontName ?? "", size: CGFloat(fontSize ?? 0.0)))
-                    .foregroundColor((foregroundColor != nil) ? foregroundColor : .black).autocorrectionDisabled()
-                    .frame(height: CGFloat(height ?? 45.0))
-                    .textFieldStyle(PlainTextFieldStyle())
-                    .padding([.horizontal], 8)
-                    .cornerRadius(16)
-                    .overlay(RoundedRectangle(cornerRadius: 16).stroke(foregroundColor ?? Color.gray.opacity(0.3)))
-                    .padding([.horizontal], 30)
+                .textCase(.lowercase)
+                .font(.custom(fontName ?? "", size: CGFloat(fontSize ?? 0.0)))
+                .foregroundColor((foregroundColor != nil) ? foregroundColor : .black).autocorrectionDisabled()
+                .frame(height: CGFloat(height ?? 45.0))
+                .textFieldStyle(PlainTextFieldStyle())
+                .padding([.horizontal], 8)
+                .cornerRadius(16)
+                .overlay(RoundedRectangle(cornerRadius: 5).stroke(foregroundColor ?? Color.gray.opacity(0.3)))
+                .padding([.horizontal], 30)
+                .disableAutocorrection(true)
+                .onChange(of: textInput) { newValue in
+                    textInput = setTextCase(text: textInput)
+                }
                 
             } else {
-                TextField(placeholder ?? "", text: $textInput, onEditingChanged: editingChanged, onCommit: commit)
-                    .textCase(.lowercase)
-                    .font(.custom(fontName ?? "", size: CGFloat(fontSize ?? 0.0)))
-                    .foregroundColor((foregroundColor != nil) ? foregroundColor : .black).autocorrectionDisabled()
-                    .frame(height: CGFloat(height ?? 45.0))
-                    .textFieldStyle(PlainTextFieldStyle())
-                    .padding([.horizontal], 8)
-                    .cornerRadius(16)
-                    .overlay(RoundedRectangle(cornerRadius: 16).stroke(foregroundColor ?? Color.gray.opacity(0.3)))
-                    .padding([.horizontal], 30)
+                TextField(placeholder ?? "", text: $textInput,onEditingChanged: { _ in
+                    textInput = setTextCase(text: textInput)
+                }, onCommit: commit)
+                .textCase(.lowercase)
+                .font(.custom(fontName ?? "", size: CGFloat(fontSize ?? 0.0)))
+                .foregroundColor((foregroundColor != nil) ? foregroundColor : .black).autocorrectionDisabled()
+                .frame(height: CGFloat(height ?? 45.0))
+                .textFieldStyle(PlainTextFieldStyle())
+                .padding([.horizontal], 8)
+                .cornerRadius(16)
+                .overlay(RoundedRectangle(cornerRadius: 5).stroke(foregroundColor ?? Color.gray.opacity(0.3)))
+                .padding([.horizontal], 30)
+                .disableAutocorrection(true)
+                .onChange(of: textInput) { newValue in
+                    textInput = setTextCase(text: textInput)
+                }
             }
         }
+    }
+}
+@available(macOS 13.15, *)
+public extension SwiftUIInputTextField {
+    func setTextCase(text: String) -> String {
+        if let textCase = textCase {
+            if textCase == .uppercase {
+                return text.lowercased()
+            } else if textCase == .lowercase {
+                return text.lowercased()
+            }
+        }
+        return text.lowercased()
     }
 }
 //:completeSettings = none
